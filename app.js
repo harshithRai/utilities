33357570f7ea9dd95eff3da5f5203884b06c1a36
harshithRai <59109406+harshithRai@users.noreply.github.com>
2020-02-09 18:32:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let body = document.getElementsByTagName("body")[0];
 let leftColor = document.querySelector("#leftColor");
 let rightColor = document.querySelector("#rightColor");
+let curBg = document.querySelector("#curBg");
 
 function setGradient() {
 	body.style.background = "linear-gradient(to right, " + leftColor.value + "," + rightColor.value + ")";
+	curBg.textContent = body.style.background + ";";
 }
 
 
